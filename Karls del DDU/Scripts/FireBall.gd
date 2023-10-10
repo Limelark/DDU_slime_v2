32724fa164d48f5e90fb7e67613069240e64a1c9
Limelark <kemobj@gmail.com>
2023-10-10 10:07:31 +0200

    test

--- Karls del DDU/Scripts/FireBall.gd
+++ Karls del DDU/Scripts/FireBall.gd
@@ -7,4 +7,12 @@ var spawnDir = "none"
 func _physics_process(delta):
 	spawnDir = Global.playerDIR
 	print(spawnDir)
+	if	spawnDir == "none":
+		velocity.y = speed * delta
+	if	spawnDir == "back":
+		velocity.y = -speed * delta
+	if	spawnDir == "left":
+		velocity.x = speed * delta
+	if	spawnDir == "right":
+		velocity.x = -speed * delta
 	
