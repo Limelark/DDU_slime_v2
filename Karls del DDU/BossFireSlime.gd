d03640613c556e0cb9080bd7312cd998f1278239
Limelark <kemobj@gmail.com>
2023-10-11 13:31:51 +0200

    down med boss

--- Karls del DDU/BossFireSlime.gd
+++ Karls del DDU/BossFireSlime.gd
@@ -1,51 +1,53 @@
 extends KinematicBody2D
 
-var speed = 0
+var speed = 30
 var chase = false
 var player = null
-var spawnSlimes = false
-var slimeTimer = true
-var health = 2000
+var fireSlimePosition = null
+var health = 100
 var in_attack_zone = false
 var dead = false
 var canTakeDamage = true
-var slimeSpawnPos = 1
-var fireSlime = preload("res://BossFireSlime.tscn")
-func _physics_process(delta):
-	print(slimeTimer)
-	
-	if	health > -999999:
-		updateHealth();
-		deadCheck()
+var fireTrail = preload("res://Scenes/fireTrail.tscn")
+var timerStarted = false
 
-		deal_with_damage();
-		if	chase == true:
-			position += (player.position - position)/speed
+
+func _physics_process(delta):
+	if health > -999999:
+		updateHealth()
+		deadCheck()
+		deal_with_damage()
+		
+		if chase == true:
+			var displacement = ((player.position) - position).normalized() * speed * delta
+			position += displacement 
+			
+			if timerStarted == false:
+				$trailTimer.start()
+				timerStarted = true
+				var fireTrailInstance = fireTrail.instance()
+				fireTrailInstance.position = position  # Set the position to be the same as the fireSlimePosition
+				get_parent().add_child(fireTrailInstance)  # Add the instance to the scene
+				get_parent().move_child(fireTrailInstance,10)
 			$AnimatedSprite.play("Walk")
-			if	player.position.x < position.x:
+			if player.position.x < position.x:
 				$AnimatedSprite.flip_h = true
 			else:
 				$AnimatedSprite.flip_h = false
 		else:
-				$AnimatedSprite.play("Idle")
+			$AnimatedSprite.play("Idle")
 	else:
 		$AnimatedSprite.play("Death")
 
-			
-		
-	
 func _on_Detection_body_entered(body):
-	spawnSlimes = true
+	player = body
+	chase = true
 	
 
+# warning-ignore:unused_argument
 func _on_Detection_body_exited(body):
-	spawnSlimes = false
-
-
-
-
-	
-
+	chase = false
+	player = null
 
 
 func deal_with_damage():
@@ -55,7 +57,7 @@ func deal_with_damage():
 			#print(health)
 			canTakeDamage = false
 			$takeDamageTimer.start()
-			print("slime took damage")
+			print("fire slime took damage")
 
 
 func _on_deathTimer_timeout():
@@ -67,10 +69,10 @@ func _on_takeDamageTimer_timeout():
 	canTakeDamage = true
 
 func updateHealth():
-	var healthBar = $slimeHealth
+	var healthBar = $fireSlimeHealth
 	
 	healthBar.value = health
-	if	health >= 2000:
+	if	health >= 100:
 		healthBar.visible = false
 	else:
 		healthBar.visible = true
@@ -79,40 +81,28 @@ func deadCheck():
 	if	health < 1 and health > -10000:
 		$deathTimer.start()
 		$AnimatedSprite.play("Death")
-		$slimeHealth.visible = false
+		$fireSlimeHealth.visible = false
 		health = -999999999
 		
-		
-func spawnSlimes():
-	if spawnSlimes == true and slimeTimer == true:
-		var fireslime = fireSlime.instance()
-		fireslime.position = $Slimespawn1.position
-		Global.slimeSpawnPos = $Slimespawn1.position
-		add_child(fireslime)
-		slimeTimer = false
-
-	
-	
-	
 
 
-func _on_slime_hitbox_area_entered(area):
+func _on_trailTimer_timeout():
+	timerStarted = false
+
+
+
+func _on_fireSlime_hitbox_area_entered(area):
 	if	area.has_method("player"):
 		in_attack_zone = true
 		print("slimesui")
 	if	area.has_method("FireBall"):
-		health -= 20
+		health -= 2
 		print("fireball zombieHit")
 		area.get_parent().queue_free()
 		updateHealth()
 
 
-func _on_slime_hitbox_area_exited(area):
+func _on_fireSlime_hitbox_area_exited(area):
 	if	area.has_method("player"):
 		in_attack_zone = false
 
-
-
-func _on_Slimespawn_timer_timeout():
-	slimeTimer = true
-	$Slimespawntimer.start()
